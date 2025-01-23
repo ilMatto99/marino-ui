e4c80d3565351343ee280211201590ebf74e43bf
Matteo <matteo99marino@gmail.com>
2025-01-23 14:50:05 +0100

    feat: button

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -1,4 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button } from '@marino-ui/button';
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
@@ -6,7 +7,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
-      <NxWelcome title="documentation-app" />
+      <Button/>
     </div>
   );
 }
--- packages/button/src/lib/button.tsx
+++ packages/button/src/lib/button.tsx
@@ -1,11 +1,9 @@
-import styles from './button.module.css';
-
-export function Button() {
+export const Button = () => {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
-    </div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
   );
-}
+};
 
 export default Button;
